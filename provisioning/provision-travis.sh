4d48bca412810014a54f9b815e4515809c1d5c56
Rosalie Tolentino <rosatolen@thoughtworks.com>
2016-05-23 10:09:14 -0500

    Updates jsunit version in provisioning script for travis

--- provisioning/provision-travis.sh
+++ provisioning/provision-travis.sh
@@ -14,7 +14,6 @@ npm install -g eslint
 sudo cp mime.types /etc/mime.types
 sudo apt-get update
 sudo apt-get install -q -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" zip thunderbird xvfb gnupg2
-#sudo apt-get upgrade -q -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold"
 wget -O /tmp/jsunit-0.1.4.xpi https://www.enigmail.net/jsunit/jsunit-0.1.4.xpi
-sudo unzip /tmp/jsunit-0.1.xpi -d /usr/lib/thunderbird-addons/extensions/jsunit@enigmail.net
+sudo unzip /tmp/jsunit-0.1.4.xpi -d /usr/lib/thunderbird-addons/extensions/jsunit@enigmail.net
 sudo /bin/bash -c "echo $TRAVIS_BUILD_DIR/build/dist > /usr/lib/thunderbird-addons/extensions/{847b3a00-7ab1-11d4-8f02-006008948af5}"
