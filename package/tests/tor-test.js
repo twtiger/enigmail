d5ab31b6833dbfecc0e4fbf844c148000870aa81
Natalie Eskinazi <n.l.eskinazi@gmail.com>
2016-06-09 16:19:10 -0500

    Making XmlHttpRequests to check.torproject.org - not yet using a proxy, may need to redo functionality

---
+++ package/tests/tor-test.js
@@ -0,0 +1,30 @@
+/*global do_load_module: false, do_get_file: false, do_get_cwd: false, testing: false, test: false, Assert: false, resetting: false, JSUnit: false, do_test_pending: false, do_test_finished: false */
+/*global Components: false, resetting: false, JSUnit: false, do_test_pending: false, do_test_finished: false, component: false, Cc: false, Ci: false */
+/*jshint -W097 */
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+"use strict";
+do_load_module("file://" + do_get_cwd().path + "/testHelper.js"); /*global withEnigmail: false, withTestGpgHome: false, getKeyListEntryOfKey: false, gKeyListObj: true */
+
+testing("tor.jsm"); /*global EnigmailTor, connect: true*/
+
+
+test(function testCheckTorRequestReturnsOk(){
+	const host = "127.0.0.1";
+  const port = "9050";	
+	let response = checkTorRequest(host, port);
+	Assert.equal(response.status, 200);
+})
+
+test(function testCheckRequestIsNotGoingThroughTorProxyWithIncorrectPort(){
+	const host = "127.0.0.1";
+  const port = "9000";	
+	let response = isTorRunning(host, port);
+	Assert.equal(response, false);
+})
+
+
--- package/tor.jsm
+++ package/tor.jsm
@@ -6,7 +6,21 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
+// TODO read in from preferences at initialization and set attributes
+// TODO implement probe for where, or if, tor is running
+// TODO check for torsocks binary
+// TODO gpg version to see if we support tor
+
 "use strict";
+
+Components.utils.import("resource://enigmail/log.jsm"); /*global EnigmailLog: false*/
+Components.utils.import("resource://gre/modules/Services.jsm"); 
+
+
+const Cc = Components.classes;
+const Ci = Components.interfaces;
+
+
 
 var EXPORTED_SYMBOLS = ["EnigmailTor"];
 
@@ -14,5 +28,19 @@ const EnigmailTor = {
 
   getConfiguration: { host: 'something', port: 'port' },
 
-  getGpgActions: { }
+  getGpgActions: {}
 };
+
+function checkTorRequest(host, port){
+  let torRequest = Cc["@mozilla.org/xmlextras/xmlhttprequest;1"].createInstance();
+  torRequest.open("GET", "https://check.torproject.org/api/ip", false);
+  torRequest.send(null);	
+  return torRequest;
+};
+
+function isTorRunning(host, port){
+	let response = checkTorRequest(host, port).response;
+	return JSON.parse(response).IsTor;
+}
+
+
