4eda0b3d660ef34848333bdc6553f89d2d4a2c0d
Rosalie Tolentino <rosatolen@thoughtworks.com>
2016-06-27 13:48:03 -0500

    Replaces old tor implementation with new tor

--- package/tests/tor-test.js
+++ package/tests/tor-test.js
@@ -8,192 +8,211 @@
 "use strict";
 do_load_module("file://" + do_get_cwd().path + "/testHelper.js"); /*global assertContains: false, withEnigmail: false, withTestGpgHome: false */
 
-testing("tor.jsm"); /*global EnigmailTor: false, connect: true, torIsAvailable: false, checkTorExists: false, TOR_IP_ADDR_PREF: false, TOR_SERVICE_PORT_PREF: false, TOR_BROWSER_BUNDLE_PORT_PREF:false, currentThread:false, HTTP_PROXY_GPG_OPTION: false, OLD_CURL_PROTOCOL:false, NEW_CURL_PROTOCOL:false, MINIMUM_WINDOWS_GPG_VERSION:false, userWantsActionOverTor:false, userRequiresTor:false, nsIEnigmail:false */
-component("enigmail/log.jsm"); /* global EnigmailLog: false */
+testing("newTor.jsm"); /*global EnigmailTor, DOWNLOAD_KEY_REQUIRED_PREF, torProperties, meetsOSConstraints, MINIMUM_WINDOWS_GPG_VERSION, MINIMUM_CURL_VERSION, createHelperArgs, gpgProxyArgs*/
 component("enigmail/prefs.jsm"); /* global EnigmailPrefs: false, REFRESH_KEY_PREF:false, DOWNLOAD_KEY_PREF:false, SEARCH_KEY_REQUIRED_PREF:false */
+component("enigmail/randomNumber.jsm"); /* global RandomNumberGenerator*/
 
-const TOR_IP_FOR_TESTS = "127.0.0.1";
-const GOOD_TOR_PORT_FOR_TEST = 9050;
-const PORT_WITHOUT_TOR = 9876;
+const DOWNLOAD_KEY_ACTION_FLAG = Ci.nsIEnigmail.DOWNLOAD_KEY;
 
-function setupGoodPortInTorServicePref() {
-  EnigmailPrefs.setPref(TOR_IP_ADDR_PREF, TOR_IP_FOR_TESTS);
-  EnigmailPrefs.setPref(TOR_SERVICE_PORT_PREF, GOOD_TOR_PORT_FOR_TEST);
-  EnigmailPrefs.setPref(TOR_BROWSER_BUNDLE_PORT_PREF, PORT_WITHOUT_TOR);
-}
-
-function setupGoodPortInBrowserBundlePref() {
-  EnigmailPrefs.setPref(TOR_IP_ADDR_PREF, TOR_IP_FOR_TESTS);
-  EnigmailPrefs.setPref(TOR_SERVICE_PORT_PREF, PORT_WITHOUT_TOR);
-  EnigmailPrefs.setPref(TOR_BROWSER_BUNDLE_PORT_PREF, GOOD_TOR_PORT_FOR_TEST);
-}
-
-test(function testCheckTorBrowserBundlePortForTor() {
-  setupGoodPortInBrowserBundlePref();
-  const executableChecker = {
-    exists: function(executable) { return false; },
-    versionOverOrEqual: function(executable, minimum) { return true; }
+test(function evaluateGpgVersionWhenOsIsWindows() {
+  const executableEvaluator = {
+    versionOverOrEqualWasCalled: false,
+    versionOverOrEqual: function(executable, minimumVersion) {
+      Assert.equal(executable, 'gpg');
+      Assert.deepEqual(minimumVersion, MINIMUM_WINDOWS_GPG_VERSION);
+      executableEvaluator.versionOverOrEqualWasCalled = true;
+      return false;
+    }
   };
 
-  const response = torIsAvailable("Linux", executableChecker);
-
-  Assert.equal(response.status, true, "Tor should be available in the TOR_IP_FOR_TESTS:TOR_BROWSER_BUNDLE_PORT_PREF");
-  Assert.equal(response.type, 'gpg-proxy');
-  Assert.equal(response.port_pref, TOR_BROWSER_BUNDLE_PORT_PREF);
+  Assert.equal(meetsOSConstraints("OS2", executableEvaluator), false);
+  Assert.equal(executableEvaluator.versionOverOrEqualWasCalled, true, "versionOverOrEqual was not called");
 });
 
-test(function testCheckForTorInServicePort() {
-  setupGoodPortInTorServicePref();
-  const executableChecker = {
-    exists: function(executable) { return false; },
-    versionOverOrEqual: function(executable, minimum) { return true; }
+test(function evaluateGpgVersionWhenOsIsWindows32() {
+  const executableEvaluator = {
+    versionOverOrEqualWasCalled: false,
+    versionOverOrEqual: function(executable, minimumVersion) {
+      Assert.equal(executable, 'gpg');
+      Assert.deepEqual(minimumVersion, MINIMUM_WINDOWS_GPG_VERSION);
+      executableEvaluator.versionOverOrEqualWasCalled = true;
+      return true;
+    }
   };
 
-  const response = torIsAvailable("Linux", executableChecker);
-
-  Assert.equal(response.status, true, "Tor is not available on " + TOR_IP_FOR_TESTS + ":" + GOOD_TOR_PORT_FOR_TEST);
-  Assert.equal(response.type, 'gpg-proxy');
-  Assert.equal(response.port_pref, TOR_SERVICE_PORT_PREF);
+  Assert.equal(meetsOSConstraints("WINNT", executableEvaluator), true);
+  Assert.equal(executableEvaluator.versionOverOrEqualWasCalled, true, "versionOverOrEqual was not called");
 });
 
-test(function testConnectingToTorFails() {
-  const portWithoutTor = 9999;
-  EnigmailPrefs.setPref(TOR_IP_ADDR_PREF, TOR_IP_FOR_TESTS);
-  EnigmailPrefs.setPref(TOR_SERVICE_PORT_PREF, portWithoutTor);
-  const executableChecker = {
-    exists: function(executable) { return false; },
-    versionOverOrEqual: function(executable, minimum) { return true; }
+
+test(function evaluateCurlVersionWhenOsIsNotWindows() {
+  const executableEvaluator = {
+    versionOverOrEqualWasCalled: false,
+    versionOverOrEqual: function(executable, minimumVersion) {
+      Assert.equal(executable, 'curl');
+      Assert.deepEqual(minimumVersion, MINIMUM_CURL_VERSION);
+      executableEvaluator.versionOverOrEqualWasCalled = true;
+      return true;
+    }
   };
 
-  const response = torIsAvailable("Linux", executableChecker);
-
-  Assert.equal(response.status, false);
+  Assert.equal(meetsOSConstraints("Linux", executableEvaluator), true);
+  Assert.equal(executableEvaluator.versionOverOrEqualWasCalled, true, "versionOverOrEqual was not called");
 });
 
-test(function checkEqualToMinimumGpgVersionInWindows() {
-  setupGoodPortInTorServicePref();
-  const executableChecker = {
-    exists: function(executable) { return false; },
-    versionOverOrEqual: function(executable, minimum) { return true; }
+test(function createHelperArgsForTorsocks() {
+  const firstSet = createHelperArgs('torsocks');
+  const secondSet = createHelperArgs('torsocks');
+
+  Assert.deepEqual(firstSet[0], '--user');
+  Assert.deepEqual(secondSet[0], '--user');
+  Assert.notEqual(firstSet[1], secondSet[1]);
+  Assert.deepEqual(firstSet[2], '--pass');
+  Assert.deepEqual(secondSet[2], '--pass');
+  Assert.notEqual(firstSet[3], secondSet[3]);
+  Assert.deepEqual(firstSet[4], '/usr/bin/gpg2');
+  Assert.deepEqual(secondSet[4], '/usr/bin/gpg2');
+});
+
+test(function createGpgProxyArgs_forWindows() {
+  const username = RandomNumberGenerator.getUint32();
+  const password = RandomNumberGenerator.getUint32();
+  const tor = {
+    ip: '127.0.0.1',
+    port: 9050,
+    username: username,
+    password: password
+  };
+  const system = {
+    isDosLikeWasCalled: false,
+    isDosLike: function() {
+      system.isDosLikeWasCalled = true;
+      return true;
+    }
   };
 
-  const response = torIsAvailable("WINNT", executableChecker);
-
-  Assert.equal(response.status, true);
-  Assert.equal(response.type, 'gpg-proxy');
-  Assert.equal(response.port_pref, TOR_SERVICE_PORT_PREF);
+  const expectedHttpProxyAddress = 'http-proxy=socks5-hostname://'+username+':'+password+'@127.0.0.1:9050';
+  Assert.deepEqual(gpgProxyArgs(tor, system), ['--keyserver-options', expectedHttpProxyAddress]);
+  Assert.equal(system.isDosLikeWasCalled, true, 'isDosLike was not called');
 });
 
-test(function checkLessThanMinimumGpgVersionInWindows() {
-  const executableChecker = {
-    exists: function(executable) { return false; },
-    versionOverOrEqual: function(executable, minimum) { return false; }
+test(function createGpgProxyArgs_forLinux() {
+  const username = RandomNumberGenerator.getUint32();
+  const password = RandomNumberGenerator.getUint32();
+  const tor = {
+    ip: '192.8.8.4',
+    port: 9150,
+    username: username,
+    password: password
+  };
+  const system = {
+    isDosLikeWasCalled: false,
+    isDosLike: function() {
+      system.isDosLikeWasCalled = true;
+      return false;
+    }
   };
 
-  const response = torIsAvailable("OS2", executableChecker);
-
-  Assert.equal(response.status, false);
+  const expectedHttpProxyAddress = 'http-proxy=socks5h://'+username+':'+password+'@192.8.8.4:9150';
+  Assert.deepEqual(gpgProxyArgs(tor, system), ['--keyserver-options', expectedHttpProxyAddress]);
+  Assert.equal(system.isDosLikeWasCalled, true, 'isDosLike was not called');
 });
 
-test(function checkForTorsocks() {
-  setupGoodPortInTorServicePref();
-  const executableChecker = {
-    exists: function(executable) { return executable === 'torsocks'; }
-  };
-
-  const response = torIsAvailable("Linux", executableChecker);
-
-  Assert.equal(response.status, true);
-  Assert.equal(response.type, 'torsocks');
+test(function returnFailure_whenUserDoesNotWantToUseTor() {
+  EnigmailPrefs.setPref(DOWNLOAD_KEY_PREF, false);
+  Assert.deepEqual(torProperties(DOWNLOAD_KEY_ACTION_FLAG), { torExists: false });
 });
 
-function setupBadPorts() {
-  EnigmailPrefs.setPref(TOR_IP_ADDR_PREF, TOR_IP_FOR_TESTS);
-  EnigmailPrefs.setPref(TOR_SERVICE_PORT_PREF, PORT_WITHOUT_TOR);
-  EnigmailPrefs.setPref(TOR_BROWSER_BUNDLE_PORT_PREF, PORT_WITHOUT_TOR);
-}
-
-test(function cannotUseTorWhenTorsocksExistsButTorNotSetup() {
-  setupBadPorts();
-  const executableChecker = {
-    exists: function(executable) { return true; }
-  };
-
-  const response = torIsAvailable("Linux", executableChecker);
-
-  Assert.equal(response.status, false);
-});
-
-test(function buildArgumentsForTorsocks() {
-  const type = { type: 'torsocks', };
-
-  const torArguments = EnigmailTor.buildGpgProxyArguments(type, "Linux");
-
-  Assert.equal(torArguments[0], "torsocks");
-  Assert.equal(torArguments[1], "--user");
-  Assert.equal(torArguments[3], "--pass");
-  Assert.equal(torArguments.length, 5);
-});
-
-test(function testBuildGpgArgumentsForTorProxy() {
-  const type = { type: 'gpg-proxy', port_pref: TOR_SERVICE_PORT_PREF };
-
-  const torArguments = EnigmailTor.buildGpgProxyArguments(type, "Linux");
-
-  Assert.equal(torArguments[0], "--keyserver-options");
-  Assert.assertContains(torArguments[1], HTTP_PROXY_GPG_OPTION+NEW_CURL_PROTOCOL);
-});
-
-function getUsername(thing) {
-  return thing[1].split(HTTP_PROXY_GPG_OPTION+NEW_CURL_PROTOCOL)[1].split(":")[0];
-}
-
-function getPassword(thing) {
-  return thing[1].split(HTTP_PROXY_GPG_OPTION+NEW_CURL_PROTOCOL)[1].split(":")[1].split("@")[0];
-}
-
-test(function createRandomUsernameAndPassword() {
-  const type = { type: 'gpg-proxy', port_pref: TOR_SERVICE_PORT_PREF };
-
-  const firstRequest = EnigmailTor.buildGpgProxyArguments(type, "Linux");
-  const secondRequest = EnigmailTor.buildGpgProxyArguments(type, "Linux");
-
-  Assert.ok(getUsername(firstRequest) !== getUsername(secondRequest));
-  Assert.ok(getUsername(firstRequest) !== getPassword(secondRequest));
-});
-
-test(function buildGpgProxyArgumentsForWindows() {
-  const type = { type: 'gpg-proxy', port_pref: TOR_SERVICE_PORT_PREF };
-
-  const torArguments = EnigmailTor.buildGpgProxyArguments(type, "OS2");
-
-  Assert.equal(torArguments[0], "--keyserver-options");
-  Assert.assertContains(torArguments[1], HTTP_PROXY_GPG_OPTION+OLD_CURL_PROTOCOL);
-});
-
-test(function buildGpgProxyArgumentsFor32bitWindows() {
-  const type = { type: 'gpg-proxy', port_pref: TOR_SERVICE_PORT_PREF };
-
-  const torArguments = EnigmailTor.buildGpgProxyArguments(type, "WINNT");
-
-  Assert.equal(torArguments[0], "--keyserver-options");
-  Assert.assertContains(torArguments[1], HTTP_PROXY_GPG_OPTION+OLD_CURL_PROTOCOL);
-});
-
-test(function actionCanHaveTor() {
+test(function returnsFailure_whenSystemCannotFindTor() {
   EnigmailPrefs.setPref(DOWNLOAD_KEY_PREF, true);
-  const actionFlags = nsIEnigmail.DOWNLOAD_KEY;
-  Assert.equal(userWantsActionOverTor(actionFlags), true);
+  const system = {
+    findTorWasCalled : false,
+    findTor: function() {
+      system.findTorWasCalled = true;
+      return {
+        exists: false
+      };
+    }
+  };
+
+  Assert.deepEqual(torProperties(DOWNLOAD_KEY_ACTION_FLAG, system), { torExists: false });
+  Assert.equal(system.findTorWasCalled, true);
 });
 
-test(function actionCannotHaveTor() {
-  EnigmailPrefs.setPref(REFRESH_KEY_PREF, false);
-  const actionFlags = nsIEnigmail.REFRESH_KEY;
-  Assert.equal(userWantsActionOverTor(actionFlags), false);
+test(function returnsSuccesWithArgs_whenAbleToFindTorAndTorsocks() {
+  EnigmailPrefs.setPref(DOWNLOAD_KEY_PREF, true);
+  const username = RandomNumberGenerator.getUint32();
+  const password = RandomNumberGenerator.getUint32();
+  const torArgs = ['--user', username, '--pass', password, '/usr/bin/gpg2'];
+  const system = {
+    findTorWasCalled: false,
+    findTor: function() {
+      system.findTorWasCalled = true;
+      return {
+        exists: true,
+        ip: '127.0.0.1',
+        port: 9050,
+        username: username,
+        password: password
+      };
+    },
+    findTorExecutableHelperWasCalled: false,
+    findTorExecutableHelper: function() {
+      system.findTorExecutableHelperWasCalled = true;
+      return {
+        exists: true,
+        command: 'torsocks',
+        args: torArgs
+      };
+    }
+  };
+
+  const properties = torProperties(DOWNLOAD_KEY_ACTION_FLAG, system);
+
+  Assert.equal(properties.torExists, true);
+  Assert.equal(properties.command, 'torsocks');
+  Assert.equal(properties.args, torArgs);
+  Assert.equal(system.findTorWasCalled, true);
+  Assert.equal(system.findTorExecutableHelperWasCalled, true);
 });
 
-test(function actionMustUseTor() {
-  EnigmailPrefs.setPref(SEARCH_KEY_REQUIRED_PREF, false);
-  const actionFlags = nsIEnigmail.SEARCH_KEY;
-  Assert.equal(userRequiresTor(actionFlags), false);
+test(function returnsSuccesWithGpgArgs_whenAbleToFindTorButNoHelpers() {
+  EnigmailPrefs.setPref(DOWNLOAD_KEY_PREF, true);
+  const username = RandomNumberGenerator.getUint32();
+  const password = RandomNumberGenerator.getUint32();
+  const gpgArgs = ['--keyserver-options', 'http-proxy=socks5h://'+username+':'+password+'@127.0.0.1:9150'];
+  const system = {
+    findTorWasCalled: false,
+    findTor: function() {
+      system.findTorWasCalled = true;
+      return {
+        exists: true,
+        ip: '127.0.0.1',
+        port: 9150,
+        username: username,
+        password: password
+      };
+    },
+    findTorExecutableHelperWasCalled: false,
+    findTorExecutableHelper: function() {
+      system.findTorExecutableHelperWasCalled = true;
+      return {
+        exists: false,
+      };
+    },
+    isDosLikeWasCalled: false,
+    isDosLike: function() {
+      system.isDosLikeWasCalled = true;
+      return false;
+    }
+  };
+
+  const properties = torProperties(DOWNLOAD_KEY_ACTION_FLAG, system);
+
+  Assert.equal(properties.torExists, true);
+  Assert.equal(properties.command, 'gpg');
+  Assert.deepEqual(properties.args, gpgArgs);
+  Assert.equal(system.findTorWasCalled, true);
+  Assert.equal(system.findTorExecutableHelperWasCalled, true);
+  Assert.equal(system.isDosLikeWasCalled, true);
 });
--- package/tor.jsm
+++ package/tor.jsm
@@ -9,12 +9,13 @@
 "use strict";
 
 Components.utils.import("resource://enigmail/log.jsm"); /*global EnigmailLog: false*/
-Components.utils.import("resource://gre/modules/XPCOMUtils.jsm"); /*global XPCOMUtils:false */
 Components.utils.import("resource://enigmail/prefs.jsm"); /* global EnigmailPrefs: false */
 Components.utils.import("resource://enigmail/randomNumber.jsm"); /* global RandomNumberGenerator: false */
 Components.utils.import("resource://enigmail/executableEvaluator.jsm"); /* global ExecutableEvaluator: false */
+Components.utils.import("resource://enigmail/os.jsm"); /* global EnigmailOS: false */
+Components.utils.import("resource://enigmail/socks5Proxy.jsm"); /* global Socks5Proxy: false */
 
-var EXPORTED_SYMBOLS = ["EnigmailTor"];
+var EXPORTED_SYMBOLS = ["NewTor"];
 
 const CC = Components.Constructor;
 const Cc = Components.classes;
@@ -33,23 +34,6 @@ const MINIMUM_WINDOWS_GPG_VERSION = {
   patch: 30
 };
 
-const VERSION_NUMERIC_BASE = 10;
-
-const LOCALHOST = "127.0.0.1";
-const LOCAL_TOR_PORT = 9050;
-const CHECK_TOR_URI = "https://check.torproject.org/api/ip";
-const CONNECTION_FLAGS = 0; // TODO: Check if we need to use TRANSPARENT_PROXY_RESOLVES_HOST flag here. Reference nsIProxyInfo documentation
-const SECONDS_TO_WAIT = -1;
-const FAILOVER_PROXY = null;
-const SHARED_CONTEXT = null;
-const LOADING_NODE = null;
-const LOADING_PRINCIPAL = null;
-const SECURITY_FLAGS = null;
-const CONTENT_POLICY = null;
-const BASE_URI = null;
-
-const EXPECTED_TOR_EXISTS_RESPONSE = "\"IsTor\":true";
-const TOR_IP_ADDR_PREF = "torIpAddr";
 const TOR_SERVICE_PORT_PREF = "torServicePort";
 const TOR_BROWSER_BUNDLE_PORT_PREF = "torBrowserBundlePort";
 const HTTP_PROXY_GPG_OPTION = "http-proxy=";
@@ -57,151 +41,14 @@ const NEW_CURL_PROTOCOL = "socks5h://";
 const OLD_CURL_PROTOCOL = "socks5-hostname://";
 
 const DOWNLOAD_KEY_PREF = "downloadKeyWithTor";
-const DOWNLOAD_KEY_REQUIRED_PREF = "downloadKeyRequireTor";
 const REFRESH_KEY_PREF = "refreshKeysWithTor";
-const REFRESH_KEY_REQUIRED_PREF = "refreshKeysRequireTor";
 const SEARCH_KEY_PREF = "searchKeyWithTor";
-const SEARCH_KEY_REQUIRED_PREF = "searchKeyRequireTor";
 const UPLOAD_KEY_PREF = "uploadKeyWithTor";
+
+const DOWNLOAD_KEY_REQUIRED_PREF = "downloadKeyRequireTor";
+const REFRESH_KEY_REQUIRED_PREF = "refreshKeysRequireTor";
+const SEARCH_KEY_REQUIRED_PREF = "searchKeyRequireTor";
 const UPLOAD_KEY_REQUIRED_PREF = "uploadKeyRequireTor";
-
-let ioservice= null;
-function createCheckTorURIChannel() {
-  if (ioservice === null) {
-    ioservice = Cc["@mozilla.org/network/io-service;1"]
-      .getService(Ci.nsIIOService);
-  }
-  return ioservice.newChannel2(CHECK_TOR_URI, "UTF-8", BASE_URI, LOADING_NODE, LOADING_PRINCIPAL, LOADING_PRINCIPAL, SECURITY_FLAGS, CONTENT_POLICY);
-}
-
-let pps = null;
-function protocolProxyService() {
-  if (pps === null) {
-    pps = Components.classes["@mozilla.org/network/protocol-proxy-service;1"]
-      .getService(Components.interfaces.nsIProtocolProxyService);
-  }
-  return pps;
-}
-
-function createScriptableInputStream(inputStream) {
-  return CC("@mozilla.org/scriptableinputstream;1", "nsIScriptableInputStream", "init")(inputStream);
-}
-
-let doneCheckingTor = false;
-let foundTor = false;
-
-const listener = {
-  onStartRequest: function(request, context) {
-    EnigmailLog.DEBUG("ON START REQUEST\n");
-  },
-  onStopRequest: function(request, context, statusCode) {
-    EnigmailLog.DEBUG("ON STOP REQUEST\n");
-    doneCheckingTor = true;
-  },
-  onDataAvailable: function(request, context, inputStream, offset, count) {
-    EnigmailLog.DEBUG("ON DATA AVAILABLE\n");
-    let response = createScriptableInputStream(inputStream).read(count);
-
-    EnigmailLog.DEBUG("RESPONSE COUNT: " + count + "\n");
-    EnigmailLog.DEBUG("RESPONSE: " + response + "\n");
-
-    foundTor = response.indexOf(EXPECTED_TOR_EXISTS_RESPONSE) !== -1;
-  },
-  QueryInterface: XPCOMUtils.generateQI([Ci.nsIRequestObserver, Ci.nsIStreamListener])
-};
-
-function filterWith(portPref) {
-  const port = EnigmailPrefs.getPref(portPref);
-  return {
-    applyFilter: function(proxyService, uri, proxyInfo) {
-      return proxyService.newProxyInfo("socks", EnigmailPrefs.getPref(TOR_IP_ADDR_PREF), port, CONNECTION_FLAGS, SECONDS_TO_WAIT, FAILOVER_PROXY);
-    },
-    QueryInterface: XPCOMUtils.generateQI([Ci.nsIProtocolProxyFilter, Ci.nsISupports])
-  };
-}
-
-function checkTorExists(filter) {
-  protocolProxyService().registerFilter(filter, 1);
-
-  createCheckTorURIChannel().asyncOpen(listener, SHARED_CONTEXT);
-  while(!doneCheckingTor) currentThread().processNextEvent(true);
-
-  let status = foundTor;
-  doneCheckingTor = false;
-  foundTor = false;
-  return status;
-}
-
-function buildGpgProxyArguments(type, os) {
-  const username = RandomNumberGenerator.getUint32();
-  const password = RandomNumberGenerator.getUint32();
-
-  if (type.type === 'torsocks') {
-    return ['torsocks', '--user', username, '--pass', password];
-  } else {
-    const args = ["--keyserver-options", HTTP_PROXY_GPG_OPTION];
-    if (os === "OS2" || os === "WINNT") {
-      args[1] += OLD_CURL_PROTOCOL;
-    } else {
-      args[1] += NEW_CURL_PROTOCOL;
-    }
-
-    const address = EnigmailPrefs.getPref(TOR_IP_ADDR_PREF);
-    const port = EnigmailPrefs.getPref(type.port_pref);
-    args[1] += username + ":" + password + "@" + address + ":" + port;
-    return args;
-  }
-}
-
-let threadManager = null;
-function currentThread() {
-  if (threadManager === null) {
-    threadManager = Cc['@mozilla.org/thread-manager;1'].getService(Ci.nsIThreadManager);
-  }
-  return threadManager.currentThread;
-}
-
-function torOnEither(browserBundlePortPref, servicePortPref) {
-  return checkTorExists(filterWith(browserBundlePortPref)) ||
-    checkTorExists(filterWith(servicePortPref));
-}
-
-function torIsAvailable(os, executableEvaluator) {
-  const failure = {
-    status: false,
-  };
-
-  if (os === "WINNT" || os === "OS2") {
-    if (!executableEvaluator.versionOverOrEqual('gpg', MINIMUM_WINDOWS_GPG_VERSION, ExecutableEvaluator.executor)) return failure;
-  } else {
-    if (executableEvaluator.exists('torsocks')) {
-      if (torOnEither(TOR_BROWSER_BUNDLE_PORT_PREF, TOR_SERVICE_PORT_PREF)) {
-        return { status: true, type: 'torsocks' };
-      } else {
-        return failure;
-      }
-    }
-    if (!executableEvaluator.versionOverOrEqual('curl', MINIMUM_CURL_VERSION, ExecutableEvaluator.executor)) return failure;
-  }
-
-  if (checkTorExists(filterWith(TOR_BROWSER_BUNDLE_PORT_PREF))) {
-    return {
-      status: true,
-      type: 'gpg-proxy',
-      port_pref: TOR_BROWSER_BUNDLE_PORT_PREF
-    };
-  }
-
-  if (checkTorExists(filterWith(TOR_SERVICE_PORT_PREF))) {
-    return {
-      status: true,
-      type: 'gpg-proxy',
-      port_pref: TOR_SERVICE_PORT_PREF
-    };
-  }
-
-  return failure;
-}
 
 const nsIEnigmail = Ci.nsIEnigmail;
 function userWantsActionOverTor(actionFlags) {
@@ -219,29 +66,135 @@ function userWantsActionOverTor(actionFlags) {
   }
 }
 
-function userRequiresTor(actionFlags) {
-  switch(actionFlags) {
-    case actionFlags & nsIEnigmail.DOWNLOAD_KEY:
-      return EnigmailPrefs.getPref(DOWNLOAD_KEY_REQUIRED_PREF) === true;
-    case actionFlags & nsIEnigmail.REFRESH_KEY:
-      return EnigmailPrefs.getPref(REFRESH_KEY_REQUIRED_PREF) === true;
-    case actionFlags & nsIEnigmail.SEARCH_KEY:
-      return EnigmailPrefs.getPref(SEARCH_KEY_REQUIRED_PREF) === true;
-    case actionFlags & nsIEnigmail.UPLOAD_KEY:
-      return EnigmailPrefs.getPref(UPLOAD_KEY_REQUIRED_PREF) === true;
-    default:
-      return false;
+function gpgProxyArgs(tor, system) {
+  // TODO establish when to use socks5-hostname on nonWindows
+  const args = ['--keyserver-options', HTTP_PROXY_GPG_OPTION];
+  if (system.isDosLike() === true) {
+    args[1] += OLD_CURL_PROTOCOL;
+  } else {
+    args[1] += NEW_CURL_PROTOCOL;
+  }
+
+  args[1] += tor.username + ":" + tor.password + "@" + tor.ip + ":" + tor.port;
+
+  return args;
+}
+
+function torOnEither(browserBundlePortPref, servicePortPref) {
+  const success = {
+    found: true,
+    ip: Socks5Proxy.torIpAddr()
+  };
+
+  if (Socks5Proxy.checkTorExists(browserBundlePortPref)) {
+    success.port = EnigmailPrefs.getPref(browserBundlePortPref);
+    return success;
+  } else if (Socks5Proxy.checkTorExists(servicePortPref)) {
+    success.port = EnigmailPrefs.getPref(servicePortPref);
+    return success;
+  }
+
+  return {
+    found: false
+  };
+}
+
+function meetsOSConstraints(os, executableEvaluator) {
+  if (['WINNT', 'OS2'].indexOf(os) > -1) {
+    return executableEvaluator.versionOverOrEqual('gpg', MINIMUM_WINDOWS_GPG_VERSION);
+  } else {
+    return executableEvaluator.versionOverOrEqual('curl', MINIMUM_CURL_VERSION);
   }
 }
 
-function getConfiguration(){ //needed for keyserver tests to run - implementation needs to be updated
-  return {};
+function createHelperArgs(helperName) {
+  // TODO: support all these torhelpers...
+  //const torHelpers = ['torsocks2', 'torsocks', 'usewithtor', 'torify'];
+  const username = RandomNumberGenerator.getUint32();
+  const password = RandomNumberGenerator.getUint32();
+  return ['--user', username, '--pass', password, '/usr/bin/gpg2'];
 }
 
-const EnigmailTor = {
-  torIsAvailable: torIsAvailable,
-  buildGpgProxyArguments: buildGpgProxyArguments,
-  userWantsActionOverTor: userWantsActionOverTor,
-  userRequiresTor: userRequiresTor,
-  getConfiguration: getConfiguration
+const systemCaller = {
+  findTor: function() {
+    const tor = torOnEither(TOR_BROWSER_BUNDLE_PORT_PREF, TOR_SERVICE_PORT_PREF);
+    EnigmailLog.CONSOLE("Tor on either browser bundle port or service port: " + tor.found + "\n");
+    if (tor.found === false || !meetsOSConstraints(EnigmailOS.getOS(), ExecutableEvaluator))
+      return { exists: false };
+    else
+      return {
+        exists: true,
+        ip: tor.ip,
+        port: tor.port,
+        username: RandomNumberGenerator.getUint32(),
+        password: RandomNumberGenerator.getUint32()
+      };
+  },
+
+  findTorExecutableHelper: function() {
+    // TODO: support all these torhelpers...
+    //const torHelpers = ['torsocks2', 'torsocks', 'usewithtor', 'torify'];
+    const torHelpers = ['torsocks'];
+    for (let i=0; i<torHelpers.length; i++) {
+      if (ExecutableEvaluator.exists(torHelpers[i]))
+        return {
+          exists: true,
+          command: torHelpers[i],
+          args: createHelperArgs(torHelpers[i])
+        };
+    }
+    return {
+      exists:false
+    };
+  },
+
+  getOS: function() {
+    return EnigmailOS.getOS();
+  },
+};
+
+function torProperties(actionFlags, system) {
+  const failure = { torExists: false };
+
+  if (userWantsActionOverTor(actionFlags) === false) return failure;
+
+  const tor = system.findTor();
+  if (tor.exists === false) return failure;
+
+  const torHelper = system.findTorExecutableHelper();
+  if (torHelper.exists === true) {
+    return {
+      torExists: tor.exists,
+      command: torHelper.command,
+      args: torHelper.args
+    };
+  }
+
+  return {
+    torExists: tor.exists,
+    command: 'gpg',
+    args: gpgProxyArgs(tor, system)
+  };
+}
+
+const NewTor = {
+  torProperties: function(actionFlags) {
+    return torProperties(actionFlags, systemCaller);
+  },
+
+  //TODO: should check for this in keyserver
+  userRequiresTor: function(actionFlags) {
+    switch(actionFlags) {
+      case actionFlags & nsIEnigmail.DOWNLOAD_KEY:
+        return EnigmailPrefs.getPref(DOWNLOAD_KEY_REQUIRED_PREF) === true;
+      case actionFlags & nsIEnigmail.REFRESH_KEY:
+        return EnigmailPrefs.getPref(REFRESH_KEY_REQUIRED_PREF) === true;
+      case actionFlags & nsIEnigmail.SEARCH_KEY:
+        return EnigmailPrefs.getPref(SEARCH_KEY_REQUIRED_PREF) === true;
+      case actionFlags & nsIEnigmail.UPLOAD_KEY:
+        return EnigmailPrefs.getPref(UPLOAD_KEY_REQUIRED_PREF) === true;
+      default:
+        return false;
+    }
+  }
 };
