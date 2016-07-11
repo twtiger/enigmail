a179c367feaa0e379239ce576cdf98225acfc690
Rosalie Tolentino <rosatolen@thoughtworks.com>
2016-07-11 09:11:32 -0500

    Sets key refresh service off for other enigmail tests that don't directly test it

--- package/tests/enigmail-test.js
+++ package/tests/enigmail-test.js
@@ -32,6 +32,7 @@ function newEnigmail(f) {
 
 // testing: initialize
 test(function initializeWillPassEnvironmentIfAskedTo() {
+  EnigmailPrefs.setPref('keyRefreshOn', false);
   var window = JSUnit.createStubWindow();
   withEnvironment({
     "ENIGMAIL_PASS_ENV": "STUFF:BLARG",
@@ -45,6 +46,7 @@ test(function initializeWillPassEnvironmentIfAskedTo() {
 });
 
 test(function initializeWillNotPassEnvironmentsNotAskedTo() {
+  EnigmailPrefs.setPref('keyRefreshOn', false);
   var window = JSUnit.createStubWindow();
   var environment = Cc["@mozilla.org/process/environment;1"].getService(nsIEnvironment);
   environment.set("ENIGMAIL_PASS_ENV", "HOME");
@@ -56,6 +58,7 @@ test(function initializeWillNotPassEnvironmentsNotAskedTo() {
 });
 
 test(function initializeWillNotSetEmptyEnvironmentValue() {
+  EnigmailPrefs.setPref('keyRefreshOn', false);
   var window = JSUnit.createStubWindow();
   var environment = Cc["@mozilla.org/process/environment;1"].getService(nsIEnvironment);
   environment.set("APPDATA", "");
