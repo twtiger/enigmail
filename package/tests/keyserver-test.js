8cc898fc8b2b759a786fe652777e3ef46a5c7926
Rosalie Tolentino <rosatolen@thoughtworks.com>
2016-07-21 11:08:53 -0500

    Adds copyright information

--- package/tests/keyserver-test.js
+++ package/tests/keyserver-test.js
@@ -1,4 +1,10 @@
 /*global test:false, component: false, testing: false, Assert: false, do_load_module: false, do_get_cwd: false */
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
 "use strict";
 
 do_load_module("file://" + do_get_cwd().path + "/testHelper.js"); /*global resetting, withEnvironment, withEnigmail: false, withTestGpgHome: false, getKeyListEntryOfKey: false, gKeyListObj: true */
