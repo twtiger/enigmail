5d6ccad2a3072827e624b014da0fc71ed8eee50c
Rosalie Tolentino <rosatolen@thoughtworks.com>
2016-08-05 12:03:37 -0500

    R: Uses enigmail as download source for jsunit-0.1.4.xpi

--- provisioning/provision-vagrant.sh
+++ provisioning/provision-vagrant.sh
@@ -12,7 +12,7 @@ apt-get install -y xfce4 virtualbox-guest-dkms virtualbox-guest-utils virtualbox
 apt-get install -y ntp
 apt-get install -y rng-tools haveged
 apt-get upgrade -y
-wget -O /tmp/jsunit-0.1.4.xpi https://olabini.se/tmp/jsunit-0.1.4.xpi
+wget -O /tmp/jsunit-0.1.4.xpi https://www.enigmail.net/jsunit/jsunit-0.1.4.xpi
 rm -rf /usr/lib/thunderbird-addons/extensions/jsunit@enigmail.net
 unzip /tmp/jsunit-0.1.4.xpi -d /usr/lib/thunderbird-addons/extensions/jsunit@enigmail.net
 rm -rf '/usr/lib/thunderbird-addons/extensions/{847b3a00-7ab1-11d4-8f02-006008948af5}'
